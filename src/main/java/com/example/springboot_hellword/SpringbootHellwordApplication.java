eb6936a49d55f71075a374ea8b4a6ac646a6eee9
admin <lyz4513@163.com>
2022-04-09 11:09:21 +0800

    [master] 创建springboot项目

---
+++ src/main/java/com/example/springboot_hellword/SpringbootHellwordApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_hellword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHellwordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHellwordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_hellword/SpringbootHellwordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_hellword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHellwordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
