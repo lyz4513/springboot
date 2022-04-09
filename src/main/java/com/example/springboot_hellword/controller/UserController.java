fc8c5cda100d974e9e61b9c7020fbe2dfa470284
admin <lyz4513@163.com>
2022-04-09 11:59:25 +0800

    [dev-1.0.1] 创建userController

--- src/main/java/com/example/springboot_hellword/controller/UserController.java
+++ src/main/java/com/example/springboot_hellword/controller/UserController.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
+
+    private static final String a = "ss";
+
+    public void user () {
+        System.out.println("a");
+    }
 }
